aaaaf2ea7c06d0848809f85f2ac5d294bc95a197
Yuriy Protsyuk <yuriy.protsyuk1999@gmail.com>
2017-11-15 17:35:33 +0200

    Asteroid shape changed to square

--- game.js
+++ game.js
@@ -635,15 +635,15 @@ AlienBullet.prototype = new Bullet();
 Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
-              -5,   7,
-              -3,   4,
-               1,  10,
-               5,   4,
+              0,   -10,
+              10,   0,
+               0,  10,
+             /*  5,   4,
               10,   0,
                5,  -6,
                2, -10,
               -4, -10,
-              -4,  -5]);
+              -4,  -5*/]);
 
   this.visible = true;
   this.scale = 6;
